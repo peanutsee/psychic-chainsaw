2902f2da18eb373cb99f892ce8b5a9c99e408bc4
peanutsee <dsee007@yahoo.com>
2024-10-13 21:34:51 +0800

    implementation of ema strategy

--- src/strategy.py
+++ src/strategy.py
@@ -7,9 +7,6 @@ class SimpleMovingAverage:
         
         self.short_lag = short_lag
         self.long_lag = long_lag
-        self.start_date = start_date
-        self.end_date = end_date
-        self.interval = interval
 
         return None
         
@@ -41,4 +38,37 @@ class SimpleMovingAverage:
         return df_sma
     
 class ExponentialMovingAverage:
+    def __init__(self, short_lag: int = 3, long_lag: int = 5) -> None:
+    
+    self.short_lag = short_lag
+    self.long_lag = long_lag
+
+    return None
+
+    def __str__(self) -> str:
+        return f"EMA({self.short_lag}, {self.long_lag}) Strategy"
+
+    def sma(self, df: pd.DataFrame = None) -> pd.DataFrame:
+        # Create window
+        df['ema_short'] = df['adjclose'].ewm(span=short_lag, adjust=False).mean()
+        df['ema_long'] = df['adjclose'].ewm(span=long_lag, adjust=False).mean()
+
+        # Create a new DataFrame with relevant columns
+        df_ema = df[['adjclose', 'ema_short', 'ema_long']].copy()
+        df_ema.rename(columns={'adjclose': 'price'}, inplace=True)
+
+        # Initialize 'Signal' column where 1 = Buy, -1 = Sell, 0 = Hold
+        df_ema['signal'] = 0
+
+        # Add Date Column
+        df_ema['date'] = df['date']
+
+        # Iterate through the DataFrame to create buy/sell signals
+        for i in range(1, len(df_ema)):
+            if df_ema['ema_short'][i] > df_ema['ema_long'][i] and df_ema['ema_short'][i-1] <= df_ema['ema_long'][i-1]:
+                df_ema.at[i, 'signal'] = 1  # Buy signal
+            elif df_ema['ema_short'][i] < df_ema['ema_long'][i] and df_ema['ema_short'][i-1] >= df_ema['ema_long'][i-1]:
+                df_ema.at[i, 'signal'] = -1  # Sell signal
+
+        return df_ema
     
